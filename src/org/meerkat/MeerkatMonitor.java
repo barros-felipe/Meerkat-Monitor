cfe47c43c4232574b8963ef69247a4cf950bac7d
Pedro Nunes <pgh.nunes@gmail.com>
2013-02-22 15:59:20 -0300

    New splash screen with progress bar
    Signed-off-by: Pedro Nunes <pgh.nunes@gmail.com>

--- src/org/meerkat/MeerkatMonitor.java
+++ src/org/meerkat/MeerkatMonitor.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.util.Properties;
 import java.util.logging.Level;
 
+import javax.swing.SwingUtilities;
 import javax.xml.ws.Endpoint;
 
 import org.apache.log4j.Logger;
@@ -57,6 +58,7 @@ public class MeerkatMonitor {
 	private static RSS rssFeed;
 	private static MasterKeyManager mkm;
 	private static MeerkatGeneralOperations mgo;
+	private static boolean isSplashSupported = false;
 
 	/**
 	 * main
@@ -64,17 +66,42 @@ public class MeerkatMonitor {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int numberStepsToProgress = 14;
+		double percentIncrease = 100 / numberStepsToProgress;
+		double currProgress = 0;
+		
+		final SplashScreen splashScreen = new SplashScreen(version);
 		// Show splash screen if available
-		log.info("Meekat-Monitor v."+version+" starting...");
-		try {
-			SplashScreen spcscreen = new SplashScreen(version);
-			spcscreen.run();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					splashScreen.showScreen();
+					isSplashSupported = true;
+				}
+				catch ( Exception e ) {
+					log.info("Desktop environment not available. [Running in console mode]");
+				}
+			}	
+		});
+		
+		if(isSplashSupported){
+			splashScreen.setProgress("Meekat-Monitor v."+version+" starting...",(int)Math.round(currProgress));
+			currProgress += percentIncrease;
 		}
-		catch ( Exception e ) {
-			log.info("Desktop environment not available. [Running in console mode]");
+		log.info("Meekat-Monitor v."+version+" starting...");
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			log.error("Faile to sleep thread! "+e1.getMessage());
 		}
 
+
 		// Setup general log settings
+		if(isSplashSupported){
+			splashScreen.setProgress("Loading settings...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Updating log setting...");
 		LogSettings ls = new LogSettings();
 		ls.setupLogGeneralOptions();
@@ -86,27 +113,47 @@ public class MeerkatMonitor {
 		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
 		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.commons.httpclient", "error");
 		System.setProperty("derby.locks.deadlockTrace", "true");
-		
+
 		// Load SQL Drivers
+		if(isSplashSupported){
+			splashScreen.setProgress("Loading JDBC Drivers and DB...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Loading JDBC Drivers...");
 		SQLDriverLoader sqlDL = new SQLDriverLoader();
 		sqlDL.loadDrivers();
 
 		// Setup embedded Database
+		if(isSplashSupported){
+			splashScreen.setProgress("Setting up embedded database...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Setting up embedded database...");
 		EmbeddedDB ebd = new EmbeddedDB();
 		// Load the driver first time
 		ebd.loadDriver();		
 		ebd.initializeDB();
-		//ebd.executeDBMaintenance();
+		if(isSplashSupported){
+			splashScreen.setProgress("Executing database maintenance...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
+		ebd.executeDBMaintenance();
 
 		// Prepare applications settings
+		if(isSplashSupported){
+			splashScreen.setProgress("Loading application settings...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Loading application settings...");
 		PropertiesLoader pL = new PropertiesLoader(propertiesFile);
 		pL.validateProperties(); // validate present properties
 		properties = pL.getPropetiesFromFile();
 
 		// Prepare temporary working directory
+		if(isSplashSupported){
+			splashScreen.setProgress("Creating applications...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Setting temporary dir: ");
 		mgo = new MeerkatGeneralOperations(configFile, version);
 		String tempWorkingDir = mgo.getTmpWorkingDir();
@@ -118,6 +165,10 @@ public class MeerkatMonitor {
 		mkm = new MasterKeyManager(propertiesFile, webAppsCollection);
 
 		// Generate applications groups
+		if(isSplashSupported){
+			splashScreen.setProgress("Creating groups...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Creating applications groups...");
 		AppGroupCollection appGroupCollection = new AppGroupCollection();
 		appGroupCollection.populateGroups(webAppsCollection);
@@ -125,6 +176,10 @@ public class MeerkatMonitor {
 		webAppsCollection.setGroupCollection(appGroupCollection); // Set group to app collection
 
 		// Setup email settings
+		if(isSplashSupported){
+			splashScreen.setProgress("Setting up email settings...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Setting up email settings...");
 		MailManager mailManager = new MailManager(propertiesFile);
 		boolean sendEmails = Boolean.parseBoolean(properties.getProperty("meerkat.email.send.emails"));
@@ -134,6 +189,10 @@ public class MeerkatMonitor {
 		}
 
 		// Create the RSS Feed
+		if(isSplashSupported){
+			splashScreen.setProgress("Setting up RSS...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Creating RSS service...");
 		rssFeed = new RSS("Meerkat Monitor", "Meerkat Monitor RSS Alerts", "", new File(mgo.getTmpWorkingDir()).getAbsolutePath());
 		rssFeed.refreshRSSFeed();
@@ -141,6 +200,10 @@ public class MeerkatMonitor {
 		rssFeed.setServerPort(webserverPort);
 
 		// Extract needed resources
+		if(isSplashSupported){
+			splashScreen.setProgress("Extracting resources...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Extracting resources...");
 		mgo.extractWebResourcesResources();
 
@@ -148,6 +211,10 @@ public class MeerkatMonitor {
 		webAppsCollection.setDB(ebd);
 
 		// Setup web server
+		if(isSplashSupported){
+			splashScreen.setProgress("Setting up embedded server...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		log.info("Setting up embedded server...");
 		String wsdlEndpoint = "http://"+hostname+":"+(webserverPort+1)+webServiceRoot;
 		String wsdlUrl = wsdlEndpoint+"?wsdl";
@@ -157,8 +224,12 @@ public class MeerkatMonitor {
 		Endpoint.publish(wsdlEndpoint, new MeerkatWebService(mkm, webAppsCollection, httpWebServer));
 		// set the httpServer to webapp collection
 		webAppsCollection.setHttpServer(httpWebServer);
-		
+
 		// Open Dashboard in default browser if available
+		if(isSplashSupported){
+			splashScreen.setProgress("Finalizing...", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+		}
 		try {
 			java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
 			java.net.URI uri = new java.net.URI("http://"+hostname+":"+webserverPort);
@@ -171,6 +242,11 @@ public class MeerkatMonitor {
 		// Start monitor
 		log.info("Setting up monitor...");
 		Monitor monitor = new Monitor(ebd, webAppsCollection, appGroupCollection, httpWebServer, rssFeed, propertiesFile);
+		if(isSplashSupported){
+			splashScreen.setProgress("Done!", (int)Math.round(currProgress));
+			currProgress += percentIncrease;
+			splashScreen.close();
+		}
 		monitor.startMonitor();
 
 	}
